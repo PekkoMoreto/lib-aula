b1a215fa3fc676555169331e100c84dd77e2a8da
PekkoMoreto <131700285+PekkoMoreto@users.noreply.github.com>
2023-08-01 16:19:01 -0300

    Add files via upload

---
+++ src/cli.js
@@ -0,0 +1,41 @@
+import fs from 'fs';
+import pegaArquivo from "./index.js";
+import chalk from 'chalk';
+import listaValidada from './http-validacao.js';
+
+const caminho = process.argv;
+
+async function imprimeLista(valida, resultado, identificador = ''){
+    if(valida){
+        console.log(chalk.yellow('Lista validada'), chalk.black.bgGreen(identificador), await listaValidada(resultado));
+    }else{   
+        console.log(chalk.yellow('Lista de links'), chalk.black.bgGreen(identificador), resultado);
+    }
+}
+
+async function processaTexto (argumentos){
+    const caminho = argumentos[2];
+    const valida = argumentos[3] === '--valida';
+
+    try{
+        fs.lstatSync(caminho);
+    }catch(erro){
+        if(erro.code === 'ENOENT'){
+            console.log(chalk.red('Não há arquivo ou diretório com esse nome.'));
+            return;
+        }
+    }
+
+    if(fs.lstatSync(caminho).isFile()){
+        const resultado = await pegaArquivo(argumentos[2]);
+        imprimeLista(valida, resultado);
+    }else if(fs.lstatSync(caminho).isDirectory()){
+        const arquivos = await fs.promises.readdir(caminho)
+        arquivos.forEach(async (nomeDeArquivo) => {
+            const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`)
+            imprimeLista(valida, lista, nomeDeArquivo)
+        })
+    }
+}
+
+processaTexto(caminho);
